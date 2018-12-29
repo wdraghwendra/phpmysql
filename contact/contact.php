d058dcaf2b5f44402f7fbe6b5fab908e84af2e62
Raghwendra Narayan Ojha <wdraghwendra@gmail.com>
2018-12-29 09:39:43 +0530

    Update contact.php

--- contact/contact.php
+++ contact/contact.php
@@ -1,12 +1,17 @@
 <?php
+// database connection code
 $con = mysqli_connect('localhost', 'root', '','db_connect');
 
+// get the post records
 $txtName = $_POST['txtName'];
 $txtEmail = $_POST['txtEmail'];
 $txtPhone = $_POST['txtPhone'];
 $txtMessage = $_POST['txtMessage'];
 
+// database insert SQL code
 $sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+
+// insert in database 
 $rs = mysqli_query($con, $sql);
 if($rs)
 {

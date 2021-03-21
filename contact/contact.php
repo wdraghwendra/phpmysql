c4990d999f9856b2755a82bb4bb84f011fa8e4ca
Raghwendra Narayan Ojha <wdraghwendra@gmail.com>
2021-03-21 10:21:22 +0530

    Update contact.php

--- contact/contact.php
+++ contact/contact.php
@@ -2,7 +2,7 @@
 // database connection code
 
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-$con = mysqli_connect('localhost', 'root', '','mydb');
+$con = mysqli_connect('localhost', 'root', '','db_contact');
 
 // get the post records
 $txtName = $_POST['txtName'];

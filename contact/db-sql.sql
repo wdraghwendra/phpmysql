6ad3396d27430d377af15238cef9be48fbcc8fc5
Raghwendra Narayan Ojha <wdraghwendra@gmail.com>
2021-03-21 10:21:01 +0530

    Update db-sql.sql

--- contact/db-sql.sql
+++ contact/db-sql.sql
@@ -1,10 +1,10 @@
 
 --
--- Database: `mydb`
+-- Database: `db_contact`
 --
 
-CREATE DATABASE IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `mydb`;
+CREATE DATABASE IF NOT EXISTS `db_contact` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `db_contact`;
 
 -- --------------------------------------------------------
 
@@ -13,10 +13,10 @@ USE `mydb`;
 --
 
 DROP TABLE IF EXISTS `tbl_contact`;
-CREATE TABLE IF NOT EXISTS `tbl_contact` (
-`id` int(11) NOT NULL,
-  `fldName` int(50) NOT NULL,
-  `fldEmail` int(150) NOT NULL,
+CREATE TABLE `tbl_contact` (
+  `id` int(11) NOT NULL,
+  `fldName` varchar(50) NOT NULL,
+  `fldEmail` varchar(150) NOT NULL,
   `fldPhone` varchar(15) NOT NULL,
   `fldMessage` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;

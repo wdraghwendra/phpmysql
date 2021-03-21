acee47e2b2cb0d9d840de38b3b94f6306e0d1056
Raghwendra Narayan Ojha <wdraghwendra@gmail.com>
2021-03-21 11:48:33 +0530

    Update contact.php

--- contact/contact.php
+++ contact/contact.php
@@ -1,10 +1,12 @@
 <?php
 // database connection code
-
+if(isset($_POST['txtName']))
+{
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 $con = mysqli_connect('localhost', 'root', '','db_contact');
 
 // get the post records
+
 $txtName = $_POST['txtName'];
 $txtEmail = $_POST['txtEmail'];
 $txtPhone = $_POST['txtPhone'];
@@ -18,5 +20,11 @@ $rs = mysqli_query($con, $sql);
 if($rs)
 {
 	echo "Contact Records Inserted";
+}
+}
+else
+{
+	echo "Are you a genuine visitor?";
+	
 }
 ?>
